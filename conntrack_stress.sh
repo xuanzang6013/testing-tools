30beaaade82acfca5db267c04eb8fbd5b7d850cc
Chen Yi <yiche@redhat.com>
2023-09-28 18:22:23 +0800

    Update testcase, add NAT stress testcase
    Signed-off-by: Chen Yi <yiche@redhat.com>

--- conntrack_stress.sh
+++ conntrack_stress.sh
@@ -2,18 +2,19 @@
 # SPDX-License-Identifier: GPL-2.0
 # This script crate huge amount of tcp/udp/sctp ipv4/ipv6 connections
 # to test/stress the connection tracking subsystem in linux kernel.
-# Monitor one iperf3 stream to reflect conntrack hash matching perform
-#
+# Monitor one iperf3 stream to reflect conntrack hash matching performance
+
+
+# Add path
+export PATH=${PWD}/src:${PATH}
 
 # Kselftest framework requirement - SKIP code is 4.
 ksft_skip=4
-sfx=$(mktemp -u "XXXXXXXX")
+sfx="-$(mktemp -u "XXXXXXXX")"
 
-l3proto=(ipv4 ipv6)
-l4proto=(tcp udp sctp)
-
-L3=${l3proto[RANDOM%2]}
-L4=${l4proto[RANDOM%3]}
+# default
+l3proto=ipv4
+l4proto=tcp
 
 usage(){
 	echo ""
@@ -30,21 +31,28 @@ usage(){
 while getopts "46tus" o
 do
 	case $o in
-		4) L3=ipv4;;
-		6) L3=ipv6;;
-		t) L4=tcp;;
-		u) L4=udp;;
-		s) L4=sctp; modprobe sctp || exit $ksft_skip;;
+		4) l3proto=ipv4;;
+		6) l3proto=ipv6;;
+		t) l4proto=tcp;;
+		u) l4proto=udp;;
+		s) l4proto=sctp; modprobe sctp || exit $ksft_skip;;
 		*) usage;;
 	esac
 done
 
-echo "*************************************************"
-echo " L3proto: $L3, L4proto: $L4"
-echo "*************************************************"
+[ -z $l3proto ] && l3proto=ipv4
+[ -z $l4proto ] && l3proto=tcp
 
-uname -a
-free -h
+
+#echo "*************************************************"
+#echo " l3protoproto   : $l3proto"
+#echo " l4protoproto   : $l4proto"
+#echo " num_serip  : $num_serip (Also threads Num)"
+#echo " num_serport: $num_serport"
+#echo " num_cliip  : $num_cliip"
+#echo " num_cliport: $num_cliport"
+#echo "*************************************************"
+
 
 cleanup()
 {
@@ -54,12 +62,21 @@ cleanup()
 	ip netns del $S 2> /dev/null
 	ip netns del $C 2> /dev/null
 	ip netns del $F 2> /dev/null
-	rm server-$sfx.log 2> /dev/null
-	rm client-$sfx.log 2> /dev/null
+	rm server$sfx.log 2> /dev/null
+	rm client$sfx.log 2> /dev/null
 
 	sysctl -q fs.file-max=${backup_file_max}
 	sysctl -q fs.nr_open=${backup_nr_open}
 	rmmodule nf_conntrack
+
+	unset num_serip
+	unset num_serport
+	unset num_cliip
+	unset num_cliport
+	unset timeout
+	unset cli_extra_opt
+	unset ser_extra_opt
+
 	echo ""
 }
 trap cleanup EXIT
@@ -76,14 +93,14 @@ rmmodule()
 	modprobe -q -r $module
 }
 
-create_topo()
+create_ns_topo()
 {
 	backup_file_max=$(sysctl -n fs.file-max)
 	backup_nr_open=$(sysctl -n fs.nr_open)
 
-	F="F-$sfx" # Forward
-	S="S-$sfx" # Server
-	C="C-$sfx" # Client
+	F="F$sfx" # Forward
+	S="S$sfx" # Server
+	C="C$sfx" # Client
 
 	# Veth Setting
 	ip netns add $F
@@ -152,18 +169,17 @@ create_topo()
 	sysctl -wq fs.nr_open=$(sysctl -n fs.file-max) || exit 1;
 }
 
-add_ipaddrs()
+set_addrs()
 {
-	# $n server ip will start $n threads
-	serIpNum=${1:-1}
-	cliIpNum=6
 	serIP4s=""
 	serIP6s=""
 	cliIP4s=""
 	cliIP6s=""
+	serPorts=""
+	cliPorts=""
 
 	# Config Server IPs
-	for i in `seq 1 $serIpNum`
+	for i in `seq 1 $num_serip`
 	do
 		ip -net $S addr add 10.1.0.$((100+i))/16 dev s_f
 		ip -net $S addr add 2001::$((100+i))/16 dev s_f nodad
@@ -174,7 +190,7 @@ add_ipaddrs()
 	serIP6s=${serIP6s%,}
 
 	# Config Client IPs
-	for i in `seq 1 $cliIpNum`
+	for i in `seq 1 $num_cliip`
 	do
 		ip -net $C addr add 10.2.0.$((100+i))/16 dev c_f
 		ip -net $C addr add 2002::$((100+i))/16 dev c_f nodad
@@ -183,15 +199,82 @@ add_ipaddrs()
 	done
 	cliIP4s=${cliIP4s%,}
 	cliIP6s=${cliIP6s%,}
+
+	serPorts="1001-$((1000+num_serport))"
+	cliPorts="5001-$((5000+num_cliport))"
+}
+
+start_flooding()
+{
+	if [ "$l3proto" == "ipv6" ]
+	then
+		local serIPs=$serIP6s
+		local cliIPs=$cliIP6s
+	else
+		local serIPs=$serIP4s
+		local cliIPs=$cliIP4s
+	fi
+
+	case "$l4proto" in
+		"tcp")  param="-t";;
+		"udp")  param="-u";;
+		"sctp") param="-s";;
+	esac
+
+	# Prefer big "-p value".(client port is the Outermost Loop in code)
+	set -x
+	ip netns exec $S timeout $timeout connect_flood_server $param -H $serIPs -P $serPorts $ser_extra_opt | tee server$sfx.log &
+	ip netns exec $C timeout $timeout connect_flood_client $param -H $serIPs -P $serPorts \
+		-h $cliIPs -p $cliPorts $cli_extra_opt | tee client$sfx.log &
+	set +x
+	wait
+
+	echo "ip netns exec $F conntrack -C"
+	ip netns exec $F conntrack -C
+
+	while pgrep connect_flood_server
+	do
+		sleep 1
+	done
+
+	while pgrep connect_flood_client
+	do
+		sleep 1
+	done
+
 }
 
 setup()
 {
-	local serIpNum=${1:-1}
+	[ -z $num_serip ]   && { echo "num_serip   need set"; exit 1;}
+	[ -z $num_serport ] && { echo "num_serport need set"; exit 1;}
+	[ -z $num_cliip ]   && { echo "num_cliip   need set"; exit 1;}
+	[ -z $num_cliport ] && { echo "num_cliport need set"; exit 1;}
+	[ -z $timeout ]     && { echo "timeout     need set"; exit 1;}
 
-	create_topo
-	add_ipaddrs $serIpNum
+	create_ns_topo
+	set_addrs
 }
+
+get_average()
+{
+	local file=$1;
+	# Excluded first and last line from calculation
+	awk 'BEGIN {
+		a[1000];
+		sum = i = n = 0;
+	}
+	/connections/ {
+		a[i] = $4;
+		i++;
+	}
+	END {
+		for (n = 1; n < (i-1); n++)
+			sum = sum + a[n];
+		print int(sum / (i - 2));
+	}' $file
+}
+
 
 enable_flowtable()
 {
@@ -228,7 +311,7 @@ EOF
 enable_nat()
 {
 	# Config NAT IPs
-        for i in `seq 1 ${cliIpNum:-6}`
+        for i in `seq 1 ${num_cliip:-6}`
         do
                 ip -net $F addr add 10.1.$((100+i)).0/16 dev f_s
                 ip -net $F addr add 2001::$((100+i)):0/16 dev f_s nodad
@@ -268,7 +351,7 @@ enable_conntrack()
 	ip netns exec $F ping 127.0.0.1 -c 1 > /dev/null
 	if [ `ip netns exec $F conntrack -C` == "0" ]
 	then
-		echo "FAIL: conntrack doesn't get enabled!"
+		echo "FAIL: conntrack doesn't enabled!"
 		exit 1
 	fi
 }
@@ -290,181 +373,69 @@ disable_conntrack()
 	fi
 }
 
-check_requires()
-{
-	which conntrack > /dev/null || { echo "SKIP, requires conntrack-tools"; exit $ksft_skip; }
-	which iperf3 > /dev/null || { echo "SKIP, requires iperf3"; exit $ksft_skip; }
-	gcc connect_flood_server.c -o connect_flood_server -lpthread
-	gcc connect_flood_client.c -o connect_flood_client -lpthread
-}
-
-start_flooding()
-{
-	local to=${1:-5}
-
-	if [ "$L3" == "ipv6" ]
-	then
-		local serIPs=$serIP6s
-		local cliIPs=$cliIP6s
-	else
-		local serIPs=$serIP4s
-		local cliIPs=$cliIP4s
-	fi
-
-	case "$L4" in
-		"tcp")  param="-t";;
-		"udp")  param="-u";;
-		"sctp") param="-s";;
-	esac
-
-	# Prefer big "-p value".(client port is the Outermost Loop in code)
-	#set -x
-	ip netns exec $S timeout $to ./connect_flood_server $param -H $serIPs -P 1001-1500 | tee server-$sfx.log &
-	ip netns exec $C timeout $to ./connect_flood_client $param -H $serIPs -P 1001-1500 \
-		-h $cliIPs -p 50001-60000 | tee client-$sfx.log &
-	#set +x
-	wait
-
-	echo "ip netns exec $F conntrack -C"
-	ip netns exec $F conntrack -C
-
-	while pgrep connect_flood_server
-	do
-		sleep 1
-	done
-
-	while pgrep connect_flood_client
-	do
-		sleep 1
-	done
-
-}
-
-get_average()
-{
-	local file=$1;
-	# Excluded first and last line from calculation
-	awk 'BEGIN {
-		a[1000];
-		sum = i = n = 0;
-	}
-	/connections/ {
-		a[i] = $4;
-		i++;
-	}
-	END {
-		for (n = 1; n < (i-1); n++)
-			sum = sum + a[n];
-		print int(sum / (i - 2));
-	}' $file
-}
-
-conntrack_perf()
-{
-	# Config the Numer of threads are used to flood
-	local threadsNum=1
-	local timeout=5
-
-	echo "*************************************************"
-	echo "*"
-	echo "* Running ${FUNCNAME[0]}() with $threadsNum threads:"
-	echo "* Measuring how much conntrack/nat will affect the"
-	echo "* connection creating rate"
-	echo "*"
-	echo "*************************************************"
-
-	setup $threadsNum
-
-	echo "----------measuring cps without conntrack----------"
-	disable_conntrack
-	start_flooding $timeout
-	local no_trk=$(get_average server-$sfx.log)
-
-	echo "----------measuring cps with enable conntrack----------"
-	enable_conntrack
-	start_flooding $timeout
-	local en_trk=$(get_average server-$sfx.log)
-	ip netns exec $F head -5 /proc/net/nf_conntrack
-
-	# Flush entries. Or in following test, the udp tracker
-	# won't treat reused port packet as new and nat won't rewrite.
-	ip netns exec $F conntrack -F
-
-	echo "----------measuring cps with NAT----------"
-	enable_nat
-	start_flooding $timeout
-	ip netns exec $F nft list ruleset
-	local en_nat=$(get_average server-$sfx.log)
-	ip netns exec $F head -5 /proc/net/nf_conntrack
-
-	p_trk=$(echo "scale=4; $en_trk/$no_trk*100;" |bc)
-	p_nat=$(echo "scale=4; $en_nat/$no_trk*100;" |bc)
-	echo "*************************************************"
-	echo "* Average Result:"
-	echo "* Disabe trk: $no_trk cps"
-	echo "* Enable trk: $en_trk cps		$p_trk%"
-	echo "* Enable nat: $en_nat cps		$p_nat%"
-	echo "*************************************************"
-
-	cleanup
-}
-
 conntrack_code_paths()
 {
-	local threadsNum=5
-	local timeout=5
+	num_serip=5  # Also threads num
+	num_cliip=6
+	num_serport=500
+	num_cliport=10000
+	timeout=5
 
 	echo "*************************************************"
 	echo "*"
-	echo "* Running ${FUNCNAME[0]}() with $threadsNum threads:"
+	echo "* Running ${FUNCNAME[0]}() with $num_serip threads:"
 	echo "* Stressing while running conntrack -E"
 	echo "*"
 	echo "*************************************************"
 
-	setup $threadsNum
+	setup
 
 	enable_conntrack
 
-	timeout -s 9 $flood_time ip netns exec $F conntrack -f $L3 -E -b 212992001 > /dev/null 2>&1 &
-	start_flooding $flood_time
+	set -x
+	timeout -s 9 $timeout ip netns exec $F conntrack -f $L3 -E -b 212992001 > /dev/null 2>&1 &
+	set +x
+	start_flooding
 
 	cleanup
 }
 
 start_iperf3()
 {
-	local timeout=$1
-	case "$L4" in
+	case "$l4proto" in
 		"tcp")	local param="";;
 		"udp")	local param="--udp -b 0";;
 		"sctp") local param="--sctp";;
 	esac
 
-        case "$L3" in
+        case "$l3proto" in
 		"ipv4") local ip="10.1.0.100";;
 		"ipv6") local ip="2001::100";;
 	esac
-	#set -x
-	ip netns exec $S iperf3 -s -B $ip -p 5201 -D
+	set -x
+	ip netns exec $S iperf3 -s -B $ip -p 5201 -D; sleep 0.3
 	ip netns exec $C iperf3 $param -c $ip -t $timeout --cport 8888 --forceflush &
-	#set +x
+	set +x
 	sleep 1
 }
 
 conntrack_steady_state()
 {
-	local threadsNum=1
-	local timeout=8
+	num_serip=5  # Also threads num
+	num_cliip=6
+	num_serport=500
+	num_cliport=10000
+	timeout=8
 
 	echo "*************************************************"
 	echo "*"
-	echo "* Running ${FUNCNAME[0]}() with $threadsNum threads: "
+	echo "* Running ${FUNCNAME[0]}() with $num_serip threads: "
 	echo "* create n flows while monitoring the throughput"
 	echo "* nf_conntrack_buckets highly affects the 'cps'"
 	echo "*"
 	echo "*************************************************"
 
-	setup $threadsNum
+	setup
 
 	# enable_conntrack or enable_nat
 	enable_conntrack
@@ -473,8 +444,8 @@ conntrack_steady_state()
 	sysctl -w net.netfilter.nf_conntrack_max=500000
 	sysctl -w net.netfilter.nf_conntrack_buckets=100000
 
-	start_iperf3 $timeout
-	start_flooding $timeout
+	start_iperf3
+	start_flooding
 	ip netns exec $F nft list ruleset
 
 	cleanup
@@ -482,19 +453,22 @@ conntrack_steady_state()
 
 conntrack_RPC_workload()
 {
-	local threadsNum=5
-	local timeout=15
+	num_serip=5  # Also threads num
+	num_cliip=6
+	num_serport=500
+	num_cliport=10000
+	timeout=15
 
 	echo "*************************************************"
 	echo "*"
-	echo "* Running ${FUNCNAME[0]}() with $threadsNum threads:"
+	echo "* Running ${FUNCNAME[0]}() with $num_serip threads:"
 	echo "* many short lived flows coming and going"
 	echo "* stress the create/insert/delete conntrack path"
 	echo "* while monitoring the throughput"
 	echo "*"
 	echo "*************************************************"
 
-	setup $threadsNum
+	setup
 
 	enable_conntrack
 
@@ -502,11 +476,11 @@ conntrack_RPC_workload()
 	# provide enough hash buckets
 	sysctl -w net.netfilter.nf_conntrack_buckets=2001000
 
-	start_iperf3 $timeout
-	start_flooding $timeout &
+	start_iperf3
+	start_flooding
 	sleep 2
 
-	local sig_cmd=$(awk -F '`' /close_soon/'{print $2}' client-$sfx.log)
+	local sig_cmd=$(awk -F '`' /close_soon/'{print $2}' client$sfx.log)
 	echo "-----------Client start closing soon: $sig_cmd---------------"
 	eval $sig_cmd
 
@@ -516,22 +490,25 @@ conntrack_RPC_workload()
 
 conntrack_flowtable()
 {
-	local threadsNum=1
-	local timeout=15
+	num_serip=5  # Also threads num
+	num_cliip=6
+	num_serport=500
+	num_cliport=10000
+	timeout=15
 
 	echo "*************************************************"
 	echo "*"
-	echo "* Running ${FUNCNAME[0]}() with $threadsNum threads: "
+	echo "* Running ${FUNCNAME[0]}() with $num_serip threads: "
 	echo "* testing the flowtable"
 	echo "*"
 	echo "*************************************************"
 
-	setup $threadsNum
+	setup
 
 	# enable_conntrack or enable_nat
 	enable_flowtable
 
-	start_iperf3 $timeout
+	start_iperf3
 
 	# 1. start a iperf3 stream.
 	# 2. create a lot of connections
@@ -548,13 +525,13 @@ conntrack_flowtable()
 		exit 1
 	fi
 
-	local sig_cmd=$(awk -F '`' /Pause/'{print $2}' client-$sfx.log)
+	local sig_cmd=$(awk -F '`' /Pause/'{print $2}' client$sfx.log)
 	echo "----------Client pause to connect: $sig_cmd----------"
 	eval $sig_cmd
 
 	sleep $((timeout/5))
 
-	local sig_cmd=$(awk -F '`' /close_all/'{print $2}' server-$sfx.log)
+	local sig_cmd=$(awk -F '`' /close_all/'{print $2}' server$sfx.log)
 	echo "----------Server start closing all connections: $sig_cmd----------"
 	eval $sig_cmd
 
@@ -562,7 +539,7 @@ conntrack_flowtable()
 	sleep $((timeout/5))
 	ip netns exec $F head -5 /proc/net/nf_conntrack
 
-	local sig_cmd=$(awk -F '`' /close_all/'{print $2}' server-$sfx.log)
+	local sig_cmd=$(awk -F '`' /close_all/'{print $2}' server$sfx.log)
 	echo "----------Server stop closing all connections: $sig_cmd----------"
 	eval $sig_cmd
 
@@ -575,11 +552,102 @@ conntrack_flowtable()
 	cleanup
 }
 
+measure_conntrack_performance()
+{
+	num_serip=1  # Also threads num
+	num_cliip=6
+	num_serport=500
+	num_cliport=10000
+	timeout=5
+
+
+	echo "*************************************************"
+	echo "*"
+	echo "* Running ${FUNCNAME[0]}() with $num_serip threads:"
+	echo "* Measuring how much conntrack/nat will affect the"
+	echo "* connection creating rate"
+	echo "*"
+	echo "*************************************************"
+
+	setup
+
+	echo "----------measuring cps without conntrack----------"
+	disable_conntrack
+	start_flooding $timeout
+	local no_trk=$(get_average server$sfx.log)
+
+	echo "----------measuring cps with enable conntrack----------"
+	enable_conntrack
+	start_flooding $timeout
+	local en_trk=$(get_average server$sfx.log)
+	ip netns exec $F head -5 /proc/net/nf_conntrack
+
+	# Flush entries. Or in following test, the udp tracker
+	# won't treat reused port packet as new and nat won't rewrite.
+	ip netns exec $F conntrack -F
+
+	echo "----------measuring cps with NAT----------"
+	enable_nat
+	start_flooding $timeout
+	ip netns exec $F nft list ruleset
+	local en_nat=$(get_average server$sfx.log)
+	ip netns exec $F head -5 /proc/net/nf_conntrack
+
+	p_trk=$(echo "scale=4; $en_trk/$no_trk*100;" |bc)
+	p_nat=$(echo "scale=4; $en_nat/$no_trk*100;" |bc)
+	echo "*************************************************"
+	echo "* Average Result:"
+	echo "* Disabe trk: $no_trk cps"
+	echo "* Enable trk: $en_trk cps		$p_trk%"
+	echo "* Enable nat: $en_nat cps		$p_nat%"
+	echo "*************************************************"
+
+	cleanup
+}
+
+nat_iptables_stress()
+{
+	echo "*************************************************"
+	echo "*"
+	echo "* Running ${FUNCNAME[0]}() with $num_serip threads: "
+	echo "* The 'CPS' should keep at high level."
+	echo "*"
+	echo "*************************************************"
+
+	num_serip=1  # Also threads num
+	num_cliip=10
+	num_serport=1
+	num_cliport=10000
+	timeout=15
+	cli_extra_opt="-c"
+
+	enable_nat_iptables()
+	{
+		# bz2196717
+	        ip -net $F addr add 10.1.$((100+i)).0/16 dev f_s
+	        ip -net $F addr add 2001::$((100+i)):0/16 dev f_s nodad
+
+		ip netns exec $F iptables -t nat -A POSTROUTING -p tcp -j MASQUERADE --random-fully
+
+		sysctl -wq net.nf_conntrack_max=1001000
+		sysctl -wq net.netfilter.nf_conntrack_buckets=$((`sysctl -n net.nf_conntrack_max`* 2))
+		sysctl -wq net.netfilter.nf_log_all_netns=1
+	}
+
+	setup
+	enable_nat_iptables
+	start_flooding
+
+	ip netns exec $F nft list ruleset
+	ip netns exec $F head -5 /proc/net/nf_conntrack
+
+	cleanup
+}
 run_all()
 {
 	# measure how conntrack will affect connections
-	# creating rate in this specific topo
-	conntrack_perf
+	# creating rate
+	measure_conntrack_performance
 
 	# stress with `conntrack -E`
 	conntrack_code_paths
@@ -593,11 +661,13 @@ run_all()
 	conntrack_RPC_workload
 
 	# stress flow offload
-	if [ x"$L4" != x"sctp" ]
+	if [ x"$l4proto" != x"sctp" ]
 	then
 		conntrack_flowtable
 	fi
+
+	# test when snat port resource exhausted TIME_WAIT entry reuse
+	nat_iptables_stress
 }
 
-check_requires
 run_all
