5550ece9648aa1e28ff66f8d8b26a83e7ef56a1b
Yi Chen <yiche@redhat.com>
2024-08-02 02:11:56 +0800

    tools: Now kernel_install.sh can recognize version, rt/debug/non
    Signed-off-by: Yi Chen <yiche@redhat.com>

--- tools/kernel_install.sh
+++ tools/kernel_install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# possible input are
+# Possible input are
 # 5.14.0-484.el9.x86_64+debug
 # 5.14.0-484.el9.x86_64+rt
 # kernel-5.14.0-484.el9
@@ -9,8 +9,7 @@
 # kernel-rt-5.14.0-484.el9
 # kernel-rt-debug-5.14.0-484.el9
 #
-# Zstream
-# kernel-5.14.0-427.29.1.el9_4
+# Zstream kernel-5.14.0-427.29.1.el9_4
 #
 #
 # 6.10.0-15.el10.x86_64
@@ -20,15 +19,38 @@
 # 3.10.0-1160.119.1.el7.x86_64
 
 input=$1
+test -z $input && {
+	echo "\$1 can't be empty"
+}
+
 _all_arch="aarch64 ppc64le s390x x86_64"
-version=$(echo $input | grep '[0-9]*\.[0-9]*\.[0-9]*\-[0-9]*\.[a-z][a-z][0-9]*') #5.14.0-484.el9
+version=$(echo $input | grep -o -E '[0-9]*\.[0-9]*\.[0-9]*\-[0-9]*(\.[0-9]*)*\.[a-z][a-z][0-9]*(_[0-9]*)?' 2>/dev/null) #5.14.0-484.el9
+
+for i in ${_all_arch}
+do
+	if [[ "$input" =~ "$i" ]]
+	then
+		arch=$i
+	fi
+done
+arch=${arch:-x86_64}
 
 echo $input | grep -q rt && {
 	bool_rt=true
+} || {
+	bool_rt=false
 }
 
 echo $input | grep -q debug && {
 	bool_debug=true
+} || {
+	bool_debug=false
 }
 
+name=kernel
+$bool_rt && name+='-rt'
+$bool_debug && name+='-debug'
 
+echo version = $version 
+echo arch = $arch
+echo name = $name
