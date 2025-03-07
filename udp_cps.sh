fa4c4d22e5f999a572feb8afa85590e233592b13
Yi Chen <yiche@redhat.com>
2025-03-07 12:03:34 +0800

    udp_cps.sh: Support setting the number of threads to be used conveniently
    Signed-off-by: Yi Chen <yiche@redhat.com>

--- udp_cps.sh
+++ udp_cps.sh
@@ -3,6 +3,10 @@
 # compare the new established CPS (connections per second)
 # of TCP and UDP.
 
+# Set the number of threads to use.
+N_THD=${N_THD:-3}
+#==========================================
+
 cleanup()
 {
         for n in $(ip netns)
@@ -31,10 +35,16 @@ ip netns exec S ip link set lo up
 ip netns exec R ip link set lo up
 ip netns exec C ip link set lo up
 
-# ipv4
-ip netns exec S ip addr add 10.167.69.1/24 dev s_r
-ip netns exec S ip addr add 10.167.69.2/24 dev s_r
-ip netns exec S ip addr add 10.167.69.3/24 dev s_r
+## ipv4
+# server ip
+for i in `seq 1 $N_THD`
+do
+	ip -net S addr add 10.167.69.$i/24 dev s_r
+	serIP4s+="10.167.69.$i,"
+done
+serIP4s=${serIP4s%,}
+
+# client ip
 ip netns exec C ip addr add 10.167.68.1/24 dev c_r
 ip netns exec C ip addr add 10.167.68.2/24 dev c_r
 ip netns exec C ip addr add 10.167.68.3/24 dev c_r
@@ -46,10 +56,16 @@ ip netns exec C ip route add 10.167.69.0/24 via 10.167.68.254 dev c_r
 ip netns exec S ip route add 10.167.68.0/24 via 10.167.69.254 dev s_r
 ip netns exec C ping 10.167.69.1 -c1 || exit 1;
 
-# ipv6
-ip netns exec S ip addr add 2001:db8:ffff:22::1/64 dev s_r nodad
-ip netns exec S ip addr add 2001:db8:ffff:22::2/64 dev s_r nodad
-ip netns exec S ip addr add 2001:db8:ffff:22::3/64 dev s_r nodad
+## ipv6
+# server ip
+for i in `seq 1 $N_THD`
+do
+	ip -net S addr add 2001:db8:ffff:22::$i/64 dev s_r nodad
+	serIP6s+="2001:db8:ffff:21::$i,"
+done
+serIP6s=${serIP6s%,}
+
+# client ip
 ip netns exec C ip addr add 2001:db8:ffff:21::1/64 dev c_r nodad
 ip netns exec C ip addr add 2001:db8:ffff:21::2/64 dev c_r nodad
 ip netns exec C ip addr add 2001:db8:ffff:21::3/64 dev c_r nodad
@@ -64,32 +80,32 @@ ip netns exec C ping 2001:db8:ffff:22::1 -c1 || exit 1;
 
 echo ""
 echo "Testing ipv4 tcp sockets"
-ip netns exec S timeout 21 connect_flood_server -t -H 10.167.69.1,10.167.69.2,10.167.69.3 -P 10000-10010 &
+ip netns exec S timeout 21 connect_flood_server -t -H ${serIP4s} -P 10000-10010 &
 sleep 1
-ip netns exec C timeout 20 connect_flood_client -t -H 10.167.69.1,10.167.69.2,10.167.69.3 -P 10000-10010 \
+ip netns exec C timeout 20 connect_flood_client -t -H ${serIP4s} -P 10000-10010 \
 		-h 10.167.68.1,10.167.68.2,10.167.68.3 -p 10000-60000 &
 wait
 
 echo ""
 echo "Testing ipv4 udp sockets"
-ip netns exec S timeout 21 connect_flood_server -u -H 10.167.69.1,10.167.69.2,10.167.69.3 -P 10000-10010 &
+ip netns exec S timeout 21 connect_flood_server -u -H ${serIP4s} -P 10000-10010 &
 sleep 1
-ip netns exec C timeout 20 connect_flood_client -u -H 10.167.69.1,10.167.69.2,10.167.69.3 -P 10000-10010 \
+ip netns exec C timeout 20 connect_flood_client -u -H ${serIP4s} -P 10000-10010 \
 		-h 10.167.68.1,10.167.68.2,10.167.68.3 -p 10000-60000 &
 wait
 
 echo ""
 echo "Testing ipv6 tcp sockets"
-ip netns exec S timeout 21 connect_flood_server -t -H 2001:db8:ffff:22::1,2001:db8:ffff:22::2,2001:db8:ffff:22::3 -P 10000-10010 &
+ip netns exec S timeout 21 connect_flood_server -t -H ${serIP6s} -P 10000-10010 &
 sleep 1
-ip netns exec C timeout 20 connect_flood_client -t -H 2001:db8:ffff:22::1,2001:db8:ffff:22::2,2001:db8:ffff:22::3 -P 10000-10010 \
+ip netns exec C timeout 20 connect_flood_client -t -H ${serIP6s} -P 10000-10010 \
 		-h 2001:db8:ffff:21::1,2001:db8:ffff:21::2,2001:db8:ffff:21::3 -p 10000-60000 &
 wait
 
 echo ""
 echo "Testing ipv6 udp sockets"
-ip netns exec S timeout 21 connect_flood_server -u -H 2001:db8:ffff:22::1,2001:db8:ffff:22::2,2001:db8:ffff:22::3 -P 10000-10010 &
+ip netns exec S timeout 21 connect_flood_server -u -H ${serIP6s} -P 10000-10010 &
 sleep 1
-ip netns exec C timeout 20 connect_flood_client -u -H 2001:db8:ffff:22::1,2001:db8:ffff:22::2,2001:db8:ffff:22::3 -P 10000-10010 \
+ip netns exec C timeout 20 connect_flood_client -u -H ${serIP6s} -P 10000-10010 \
 		-h 2001:db8:ffff:21::1,2001:db8:ffff:21::2,2001:db8:ffff:21::3 -p 10000-60000 &
 wait
